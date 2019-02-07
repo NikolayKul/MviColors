5b56f2d7372f75722aa835536352e3fd33a765c6
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2019-02-07 14:52:19 +0300

    Fix appId

--- app/src/androidTest/java/com/nikolaykul/mvicolors/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/nikolaykul/mvicolors/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.nikolaykul.shortvids", appContext.packageName)
+        assertEquals("com.nikolaykul.mvicolors", appContext.packageName)
     }
 }
